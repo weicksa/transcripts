b6a338ad2330536cfcef45328484fbc5e0b4494b
weicksa <75697263+weicksa@users.noreply.github.com>
2026-01-15 12:58:27 +0100

    Add files via upload

---
+++ whisper_test.py
@@ -0,0 +1,32 @@
+import torch
+import sys
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+
+if __name__ == "__main__":
+
+    sample = sys.argv[1]
+    print(sample)
+    result = pipe(sample)
+
+    print(result["text"])
