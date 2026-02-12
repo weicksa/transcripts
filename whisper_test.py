cc0053a8dfba19ea5495a268f3932deb213c96c4
Sandro <weicksa@web.de>
2026-02-12 10:38:15 +0100

    made code more functional

--- whisper_test.py
+++ whisper_test.py
@@ -2,31 +2,44 @@ import torch
 import sys
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "openai/whisper-large-v3"
+def run_model_on_file(data, model="openai/whisper-large-v3-turbo"):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True)
-model.to(device)
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-processor = AutoProcessor.from_pretrained(model_id)
+    model_id = "openai/whisper-large-v3-turbo"
 
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    torch_dtype=torch_dtype,
-    device=device,
-)
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    result = pipe(data)
+    return result
 
 
 if __name__ == "__main__":
 
-    sample = sys.argv[1]
-    print(sample)
-    result = pipe(sample)
+    data = sys.argv[1]
+    print(data)
+
+    print("--- Running Whisper ---")
+    result = run_model_on_file(data)
+    with open(f"{data}_transcript.txt", "w") as output:
+        output.write(result["text"])
+        print("--- Saved transcript ---")
 
     print(result["text"])
