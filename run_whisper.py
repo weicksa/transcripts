dadfda5ff62662818ef91abbc2ed74eb544afffd
Sandro <weicksa@web.de>
2026-02-17 11:25:23 +0100

    readded python script

---
+++ run_whisper.py
@@ -0,0 +1,47 @@
+import torch
+import sys
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+
+def run_model_on_file(data, model="openai/whisper-large-v3-turbo"):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    model_id = "openai/whisper-large-v3-turbo"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    result = pipe(data, return_timestamps=True)
+    return result
+
+
+if __name__ == "__main__":
+
+    data = sys.argv[1]
+    save_folder = sys.argv[2]
+
+    name = data[:-4]
+
+    print("--- Running Whisper ---")
+    result = run_model_on_file(data)
+    name = data[:-4]
+    print(name)
+    with open(f"{save_folder}/{name}_transcript.txt", "w") as output:
+        output.write(result["text"])
+        print("--- Saved transcript ---")
