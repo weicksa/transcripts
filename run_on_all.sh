7982119f36fdc8319ff610d94b1d68c39dbdc794
Sandro <weicksa@web.de>
2026-02-17 10:43:29 +0100

    path testing

--- run_on_all.sh
+++ run_on_all.sh
@@ -3,5 +3,5 @@
 mkdir ../transcripts_results
 for f in *.mp3;
 do
-  python ../whisper_test.py ${f%.mp3} ../transcripts_results;
+  python ../whisper_test.py ../min_data/${f%.mp3} ../transcripts_results;
 done
